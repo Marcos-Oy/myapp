4e2d9448f71491bc3a40e130dc636446d07c4574
Marcos Alberto Oyarzo Alvarez <oyarzo.marcos97@outlook.com>
2023-03-17 19:03:43 -0300

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'marcosoyarzo/myapp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '63c4a4c6befd8737609fbaa74606bd47f2af13aa',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'marcosoyarzo/myapp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '63c4a4c6befd8737609fbaa74606bd47f2af13aa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
